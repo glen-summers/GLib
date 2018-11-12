eac5c4d26277b253053f0d4651aeb65d5627cd48
Glen Summers <glen.summers@outlook.com>
2018-11-12 19:13:10 +0000

    add go.sh

---
+++ go.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+error_exit() {
+	echo "$1" 1>&2
+	exit 1
+}
+
+clear
+cd "$( dirname "${BASH_SOURCE[0]}" )"
+
+cmake . -DCMAKE_INSTALL_PREFIX:PATH="./install" || error_exit "cmake failed"
+cmake --build . || error_exit "cmake build failed"
+cmake --build . --target install || error_exit "cmake install failed"
