a73f1a1a23d264d0086574015791c3bec2b9a6f2
Glen Summers <glen.summers@outlook.com>
2018-11-14 17:47:10 +0000

    use ctest

--- go.sh
+++ go.sh
@@ -10,4 +10,5 @@ cd "$( dirname "${BASH_SOURCE[0]}" )"
 
 cmake . -DCMAKE_INSTALL_PREFIX:PATH="./install" || error_exit "cmake failed"
 cmake --build . || error_exit "cmake build failed"
+CTEST_OUTPUT_ON_FAILURE=1 cmake --build . --target test || error_exit "cmake build failed"
 cmake --build . --target install || error_exit "cmake install failed"
