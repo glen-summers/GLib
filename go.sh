18016380332255a47afd6a274d405324c5578ea0
Glen Summers <glen.summers@outlook.com>
2018-11-15 16:28:00 +0000

    using build\install vars ignore local build\install files using local cmake_build\cmake_install dirs nix clean

--- go.sh
+++ go.sh
@@ -5,12 +5,37 @@ error_exit() {
 	exit 1
 }
 
-clear
-cd "$( dirname "${BASH_SOURCE[0]}" )"
+Build() {
+	clear
+	mkdir -p "${CMakeBuildDir}" || error_exit "mkdir ${CMakeBuildDir} failed"
+	(cd "${CMakeBuildDir}" && cmake -DCMAKE_INSTALL_PREFIX:PATH="${CMakeInstallDir}" "${RootDir}" || error_exit "cmake failed")
+	cmake --build "${CMakeBuildDir}" || error_exit "cmake build failed"
+	CTEST_OUTPUT_ON_FAILURE=1 cmake --build "${CMakeBuildDir}" --target test || error_exit "cmake build failed"
+	cmake --build "${CMakeBuildDir}" --target install || error_exit "cmake install failed"
+}
 
-# todo, clean install
+Clean() {
+	rm -f -r -v "${CMakeBuildDir}" || error_exit "rm ${CMakeBuildDir} failed"
+	rm -f -r -v "${CMakeInstallDir}" || error_exit "rm ${CMakeInstallDir} failed"
+}
 
-cmake . -DCMAKE_INSTALL_PREFIX:PATH="./install" || error_exit "cmake failed"
-cmake --build . || error_exit "cmake build failed"
-CTEST_OUTPUT_ON_FAILURE=1 cmake --build . --target test || error_exit "cmake build failed"
-cmake --build . --target install || error_exit "cmake install failed"
+RootDir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+CMakeInstallDir="${RootDir}/cmake_install"
+CMakeBuildDir="${RootDir}/cmake_build"
+
+echo "go $1..."
+case "$1" in
+	"build")
+		;&
+	"")
+		Build
+		;;
+	clean)
+		Clean
+		;;
+	*)
+		error_exit "Usage: $0 {build*|clean}"
+esac
+
+
+# todo, clean
