ca405c8391d970ecf68208991d48d46d8f68c331
Glen Summers <glen.summers@outlook.com>
2018-11-19 20:00:42 +0000

    set shell files as lf add clone test option for wsl

--- go.sh
+++ go.sh
@@ -18,6 +18,14 @@ Clean() {
 	rm -f -r -v "${RootDir}/out" || error_exit "rm ${RootDir}/out failed"
 }
 
+Clone() {
+	local CloneDir="${RootDir}/out/GLibClone"
+	rm -f -r "${CloneDir}" || error_exit "rm ${CloneDir} failed"
+	git clone "${RootDir}" "${CloneDir}" || error_exit "git clone failed"
+	"${CloneDir}/go.sh" || error_exit "cloned go.sh failed"
+	rm -f -r "${CloneDir}" || error_exit "rm ${CloneDir} failed"
+}
+
 RootDir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 CMakeInstallDir="${RootDir}/out/cmake/install"
 CMakeBuildDir="${RootDir}/out/cmake/build"
@@ -31,6 +39,9 @@ case "$1" in
 		;;
 	clean)
 		Clean
+		;;
+	clone)
+		Clone
 		;;
 	*)
 		error_exit "Usage: $0 {build*|clean}"
