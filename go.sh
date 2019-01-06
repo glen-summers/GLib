6af20e0ed9b1d77585f514fbf6cebf5140731261
Glen Summers <glen.summers@outlook.com>
2019-01-06 19:04:59 +0000

    add lcov\genhtml coverage report

--- go.sh
+++ go.sh
@@ -6,16 +6,20 @@ error_exit() {
 }
 
 Build() {
-	CMakeInstallDir="${RootDir}/out/cmake/install/${Configuration}"
 	CMakeBuildDir="${RootDir}/out/cmake/build/${Configuration}"
-
-	clear
+	CMakeInstallDir="${RootDir}/out/cmake/install/${Configuration}"
 	mkdir -p "${CMakeBuildDir}" || error_exit "mkdir ${CMakeBuildDir} failed"
 	(cd "${CMakeBuildDir}" && cmake -DCMAKE_INSTALL_PREFIX:PATH="${CMakeInstallDir}" -DCMAKE_BUILD_TYPE=${Configuration} "${RootDir}" || error_exit "cmake failed")
-	cmake --build "${CMakeBuildDir}" --config $(Configuration) || error_exit "cmake build failed"
-	CTEST_OUTPUT_ON_FAILURE=1 cmake --build "${CMakeBuildDir}" --config $(Configuration) --target test || error_exit "cmake build failed"
-	cmake --build "${CMakeBuildDir}" --target install || error_exit "cmake install failed"
+	cmake --build "${CMakeBuildDir}" --config ${Configuration} || error_exit "cmake build failed"
+}
+
+RunTests() {
+	CTEST_OUTPUT_ON_FAILURE=1 cmake --build "${CMakeBuildDir}" --config ${Configuration} --target test || error_exit "cmake build failed"
 	cat ${CMakeBuildDir}/Testing/Temporary/LastTest.log || error_exit "test log missing"
+}
+
+Install() {
+	cmake --build "${CMakeBuildDir}" --target install || error_exit "cmake install failed"
 }
 
 Clean() {
@@ -25,16 +29,31 @@ Clean() {
 RootDir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 Configuration=Release
 
+clear
 echo "go $1..."
 case "$1" in
 
-	debug)
-		Configuration=Debug
+	"")
 		;&
 	"build")
-		;&
-	"")
 		Build
+		RunTests
+		Install
+		;;
+	coverage)
+		Configuration=Debug
+		Build		
+		
+		Name="${CMakeBuildDir}/TestsCoverage"
+		
+		lcov -directory "${CMakeBuildDir}" --zerocounters || error_exit "lcov init"
+		lcov -c -i -d "${CMakeBuildDir}" -o "${Name}.Base" || error_exit "lcov base"
+		RunTests
+		lcov -directory "${CMakeBuildDir}" --capture --output-file "${Name}.Info" || error_exit "lcov info"
+		lcov -a "${Name}.Base" -a "${Name}.Info" --output-file "${Name}.Total" || error_exit "lcov total"
+		lcov --remove "${Name}.Total" '/usr/include/*' '*/boost/*' '*/Tests/*' --output-file "${Name}.info.cleaned" || error_exit "lcov remove"
+		genhtml -o "${Name}" "${Name}.info.cleaned" || error_exit "genhtml"
+		echo "html coverage generated at: ${CMakeBuildDir}/TestsCoverage/index.html"
 		;;
 	clean)
 		Clean
