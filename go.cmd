3896f765db7fb9356cfe18f7cce6ab915fc42f7f
Glen Summers <glen.summers@outlook.com>
2019-04-21 13:50:11 +0100

    escape version number trim go

--- go.cmd
+++ go.cmd
@@ -1,33 +1,21 @@
-@echo off
-cls
-setlocal EnableDelayedExpansion
+@echo off & cls & setlocal
 
-set vsVersion="16"
+set vsVersion=(16^^^^,17]
 set buildFile=%~dp0msvc\build.build
 
 set requires=Microsoft.Component.MSBuild Microsoft.VisualStudio.Component.VC.Tools.x86.x64
-set findMsb=MSBuild\**\Bin\amd64\MSBuild.exe
+set vsWhereBaseCmd="%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe" -latest -version %vsVersion% -requires %requires% 
 
-set vsWhereDir=%ProgramFiles(x86)%\Microsoft Visual Studio\Installer
-set vsWhereBaseCmd=.\vswhere.exe -latest -requires %requires% -version %vsVersion%
-
-pushd "%vsWhereDir%" || echo vsWhere not found && exit /b 1
-for /F "tokens=* usebackq delims=" %%i in (`%vsWhereBaseCmd% -find %findMsb%`) do set msbuild=%%i
 for /F "tokens=* usebackq delims=" %%i in (`%vsWhereBaseCmd% -property installationPath`) do set vsInstallationPath=%%i
-popd
-
-if "%msbuild%" equ "" echo msbuild not found & exit /b 1
-if "%vsInstallationPath%" equ "" echo visual studio not found & exit /b 1
-
-echo msbuild=%msbuild%
+if "%vsInstallationPath%" equ "" echo Visual studio not found & exit /b 1
 echo vsInstallationPath=%vsInstallationPath%
+
+for /F "tokens=* usebackq delims=" %%i in (`%vsWhereBaseCmd% -find MSBuild\**\Bin\amd64\MSBuild.exe`) do set msbuild=%%i
+if "%msbuild%" equ "" echo msbuild not found & exit /b 1
+echo msbuild=%msbuild%
 
 set args=%*
 if "%1" NEQ "" set args=/t:%*
 
 call "%vsInstallationPath%\VC\Auxiliary\Build\vcvars64.bat"
-echo include:
-for %%A in ("%include:;=";"%") do (echo %%~A)
-
 "%msbuild%" /p:vsInstallationPath="%vsInstallationPath%" %buildFile% %args%
-exit /b 0
